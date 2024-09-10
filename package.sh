20cad03d2708d59e9b962a909376bd83d79c7dc6
ilushian <125647240+ilushian@users.noreply.github.com>
2024-09-10 19:36:57 +0000

    Update package.sh
    update

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 paru -Syu --skipreview --noconfirm -y
-paru -S sway network-manager-applet blueman dunst rofi-lbonn-wayland-git jd  waybar pyprland swww cliphist polkit-kde-agent xdg-desktop-portal-hyprland brightnessctl pavucontrol pamixer nwg-look fcitx5-bamboo-git fcitx5-gtk-git fcitx5-configtool --skipreview --noconfirm -y
+paru -S sway network-manager-applet blueman dunst rofi-lbonn-wayland-git jd-tool   waybar swww cliphist polkit-kde-agent xdg-desktop-portal-hyprland brightnessctl pavucontrol pamixer nwg-look fcitx5-bamboo-git fcitx5-gtk-git fcitx5-configtool --skipreview --noconfirm -y
 
